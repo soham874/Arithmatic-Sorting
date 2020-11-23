4caabf8ea8eb0d5be55111c91082c3cc82518e02
soham874 <soham874@gmail.com>
2020-11-23 11:28:52 +0530

    Added : Compute result of a*b+c

--- ArithmaticSorting.sh
+++ ArithmaticSorting.sh
@@ -4,6 +4,8 @@ read -p "Enter the number a : " a
 read -p "Enter the number b : " b
 read -p "Enter the number c : " c
 
-res=$(($a+($b*$c)))
+res1=$(($a+($b*$c)))
+res2=$((($a*$b)+$c))
 
-echo "Result is $res"
+echo "Result of a+b*c is $res1"
+echo "Result of a*b+c is $res2"
