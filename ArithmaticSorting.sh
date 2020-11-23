d1927be1adf21f4fca67b02ee920fef3755a0402
soham874 <soham874@gmail.com>
2020-11-23 12:08:15 +0530

    Added:Storing and ooutputting results in descending order

--- ArithmaticSorting.sh
+++ ArithmaticSorting.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+function bubblesort(){
+	arr=("$@")
+	for i in ${!arr[@]}
+	do
+		for j in ${!arr[@]}
+		do
+			if [ ${arr[$i]} -gt ${arr[$j]} ]
+			then
+				temp=${arr[$i]}
+				arr[$i]=${arr[$j]}
+				arr[$j]=$temp
+			fi
+		done
+	echo ${arr[@]}
+	done
+	echo "Sorted result array in descendin is : " ${arr[@]}
+}
+
 read -p "Enter the number a : " a
 read -p "Enter the number b : " b
 read -p "Enter the number c : " c
@@ -17,10 +35,12 @@ do
 	resultsarray[(($i-2))]=${results[$i]}
 done
 
-echo "Stored results in dictionary are : "${results[@]}
-echo "Stored results in array are : "${resultsarray[@]}
+#echo "Stored results in dictionary are : "${results[@]}
+#echo "Stored results in array are : "${resultsarray[@]}
 
 echo "Result of a+b*c is $res2"
 echo "Result of a*b+c is $res3"
 echo "Result of c+a/b is $res4"
 echo "Result of a%b+c is $res5"
+
+bubblesort "${resultsarray[@]}"
