1ffa8372b92f44ef469add03b2cbd630d85980ab
soham874 <soham874@gmail.com>
2020-11-23 11:32:06 +0530

    Added : Computing result of c+a/b

--- ArithmaticSorting.sh
+++ ArithmaticSorting.sh
@@ -4,8 +4,10 @@ read -p "Enter the number a : " a
 read -p "Enter the number b : " b
 read -p "Enter the number c : " c
 
-res1=$(($a+($b*$c)))
-res2=$((($a*$b)+$c))
+res2=$(($a+($b*$c)))
+res3=$((($a*$b)+$c))
+res4=$(($c+($a/$b)))
 
-echo "Result of a+b*c is $res1"
-echo "Result of a*b+c is $res2"
+echo "Result of a+b*c is $res2"
+echo "Result of a*b+c is $res3"
+echo "Result of c+a/b is $res4"
