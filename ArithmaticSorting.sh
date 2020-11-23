abe28baf7d7fb8c11a90c26dfdfd2d84a14bbc61
soham874 <soham874@gmail.com>
2020-11-23 11:40:45 +0530

    Refactored : Now storing the results in a dictionary

--- ArithmaticSorting.sh
+++ ArithmaticSorting.sh
@@ -9,6 +9,15 @@ res3=$((($a*$b)+$c))
 res4=$(($c+($a/$b)))
 res5=$((($a%$b)+$c))
 
+declare -A results
+
+for i in {2..5}
+do
+	results[$i]=`echo "$((res$i))"`
+done
+
+echo "Stored results are : "${results[@]}
+
 echo "Result of a+b*c is $res2"
 echo "Result of a*b+c is $res3"
 echo "Result of c+a/b is $res4"
